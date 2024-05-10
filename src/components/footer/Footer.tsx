4a281b6052633a4883538eb26ea6361aa7234f10
Denis-Hish <denis.hish@gmail.com>
2024-05-10 15:17:53 +0200

    First commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import './footer.scss';
+
+const Footer = () => {
+   return (
+      <div className='footer'>
+         <span>DenWebDev</span>
+         <span>
+            Copyright &copy; 2024 Dashboard | Created by &nbsp;
+            <a href='https://www.facebook.com/denis.nazaroff/' target='_blank'>
+               Denis Nazarow
+            </a>
+         </span>
+      </div>
+   );
+};
+
+export default Footer;
