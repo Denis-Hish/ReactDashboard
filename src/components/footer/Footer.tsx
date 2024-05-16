fb2481a29b70cd1b721be3cd209c34601c517656
Denis-Hish <denis.hish@gmail.com>
2024-05-16 10:19:57 +0200

    Added responsiveness

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,9 +3,9 @@ import './footer.scss';
 const Footer = () => {
    return (
       <div className='footer'>
-         <span>DenWebDev</span>
+         <span>&lt; DenWebDev /&gt;</span>
          <span>
-            Copyright &copy; 2024 Dashboard | Created by &nbsp;
+            Copyright &copy; 2024 Dashboard | Created by&nbsp;
             <a href='https://www.facebook.com/denis.nazaroff/' target='_blank'>
                Denis Nazarow
             </a>
